3aceb57ed00eee9b77935f17b3bb0029ecdc8d0f
peter <s0931052089@gmail.com>
2017-11-12 23:40:17 +0800

    新增單欄式網頁資料夾

---
+++ exercise.js
@@ -0,0 +1,6 @@
+/*
+use this file to write your solution and then share the solution with me.
+Exercises are available in the text files at the end of each section.
+Instructions are at the top of the text file and 
+in the videos JavaScript coding exercises 1 and 2 in the section Introduction
+*/
